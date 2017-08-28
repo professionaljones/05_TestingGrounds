c64806ae7ceb754b3507cd28efb1cb196cdd080f
Brandon Jones <bcjones1@fullsail.edu>
2017-08-28 08:06:58 -0400

    Fixed file locations, added a new Substance, but nothing really new

--- Source/S05_TestingGrounds/S05_TestingGroundsGameMode.cpp
+++ Source/S05_TestingGrounds/S05_TestingGroundsGameMode.cpp
@@ -9,9 +9,9 @@ AS05_TestingGroundsGameMode::AS05_TestingGroundsGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Character/Behaviour/BP_Character"));
-	DefaultPawnClass = PlayerPawnClassFinder.Class;
+	//static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Character/Behaviour/BP_Character"));
+	//DefaultPawnClass = PlayerPawnClassFinder.Class;
 
-	// use our custom HUD class
-	HUDClass = AS05_TestingGroundsHUD::StaticClass();
+	//// use our custom HUD class
+	//HUDClass = AS05_TestingGroundsHUD::StaticClass();
 }
